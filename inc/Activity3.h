748bf42a91c5fcd4908d5ea3004de9774c6a35f1
NisargaPriyadarshini <73154569+NisargaPriyadarshini@users.noreply.github.com>
2021-04-30 19:50:03 +0530

    Create Activity3.h

---
+++ inc/Activity3.h
@@ -0,0 +1,38 @@
+  
+#ifndef _ACTIVITY3_H_
+#define _ACTIVITY3_H_
+
+/**
+ * @file activity3.h
+ * @author Swapnil Khandekar ()
+ * @brief Activity3 in the embedded case study
+ * @version 0.1
+ * @date 2021-04-23
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+/**
+ * Macro definitions
+ * 
+ */
+
+#define DUTY_CYCLE(Percent) (1024*Percent/100) /**< DUTY_CYCLE macro function */
+#define WRITE_PWM OCR1A /**< Write PWM with OCR1A */
+
+/**
+ * @brief Function to initialize PWM    
+ * 
+ */
+void InitializePWM(void);
+
+/**
+ * @brief Function to Generate PWM    
+ * 
+ * @param[in] Temperature input temperature obtained from ADC 
+ * 
+ * @return TempToCAN
+ */
+char GeneratePWM(uint16_t Temperature);
+#endif  /* _ACTIVITY3_H_ */
